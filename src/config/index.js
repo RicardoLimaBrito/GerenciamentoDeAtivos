936d2c818b40bd1ec745642d093eae8a4268cbb8
JackTorresR <jacktorresr@gmail.com>
2020-05-22 19:14:12 -0300

    Atualizei as configurações para o banco de dados.

--- src/config/index.js
+++ src/config/index.js
@@ -5,6 +5,6 @@ export const FIREBASE = {
     projectId: "gerenciamentodeativosestacio",
     storageBucket: "gerenciamentodeativosestacio.appspot.com",
     messagingSenderId: "449508000835",
-    appId: "1:449508000835:web:d25039df35c825ace4c0f6",
-    measurementId: "G-KD8G3GBS8R"
-};
+    appId: "1:449508000835:web:21f031fdda3d563be4c0f6",
+    measurementId: "G-4DH3B4F09H"
+}
